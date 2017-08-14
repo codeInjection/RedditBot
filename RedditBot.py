c4ef531804627a52c96e1b0dc04a2ddcd7021a0c
Daniyal <danyal.aasif@gmail.com>
2017-08-15 03:23:32 +0500

    Fetch Data Function Creating

--- RedditBot.py
+++ RedditBot.py
@@ -17,3 +17,22 @@ def authenticate():
     reddit = praw.Reddit("explainbot", user_agent='web:xkcd-explain-bot:v0.1 ')
     print('Authenticated as {}\n'.format(reddit.user.me()))
     return reddit
+
+def fetchdata(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    tag = soup.find('p')
+    data = ''
+    while True:
+        if isinstance(tag, bs4.element.Tag):
+            if tag.name == 'h2':
+                break
+            if tag.name == 'h3':
+                tag = tag.nextSibling
+            else:
+                data = data + '\n' + tag.text
+                tag = tag.nextSibling
+        else:
+            tag = tag.nextSibling
+    return data
