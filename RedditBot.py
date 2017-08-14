bf4f0395f50bb82203da52fa27d5e00ae77b6f25
Daniyal <danyal.aasif@gmail.com>
2017-08-15 03:16:24 +0500

    Creating Authenticate Function

--- RedditBot.py
+++ RedditBot.py
@@ -1,1 +1,19 @@
-#First Commit Test
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+import praw
+import time
+import re
+import requests
+import bs4
+
+
+header = "**Explanation of this xkcd**\n"
+footer = '\n*---This explanation was extracted from [explainxkcd](http://www.explainxkcd.com)*'
+
+
+def authenticate():
+    print("Authenticating...\n")
+    reddit = praw.Reddit("explainbot", user_agent='web:xkcd-explain-bot:v0.1 ')
+    print('Authenticated as {}\n'.format(reddit.user.me()))
+    return reddit
