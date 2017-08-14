fb5546913db460d0e50ce8734b623f1f23aa2efc
Daniyal <danyal.aasif@gmail.com>
2017-08-15 03:36:55 +0500

    Creating run_explainbot function

--- RedditBot.py
+++ RedditBot.py
@@ -7,7 +7,7 @@ import re
 import requests
 import bs4
 
-
+path = "commented.txt"
 header = "**Explanation of this xkcd**\n"
 footer = '\n*---This explanation was extracted from [explainxkcd](http://www.explainxkcd.com)*'
 
@@ -36,3 +36,49 @@ def fetchdata(url):
         else:
             tag = tag.nextSibling
     return data
+
+def run_explainbot(reddit):
+    
+    print("Getting 250 comments...\n")
+    
+    for comment in reddit.subreddit('test').comments(limit = 250):
+        match = re.findall("[a-z]*[A-Z]*[0-9]*https://www.xkcd.com/[0-9]+", comment.body)
+        if match:
+            print("Link found in comment with comment ID: " + comment.id)
+            xkcd_url = match[0]
+            url_obj = urlparse(xkcd_url)
+            xkcd_id = int((url_obj.path.strip("/")))
+            myurl = 'http://www.explainxkcd.com/wiki/index.php/' + str(xkcd_id)
+            
+            file_obj_r = open(path,'r')
+                        
+            try:
+                explanation = fetchdata(myurl)
+            except:
+                print('Exception!!! Possibly incorrect xkcd URL...\n')
+                # Typical cause for this will be a URL for an xkcd that does not exist (Example: https://www.xkcd.com/772524318/)
+            else:
+                if comment.id not in file_obj_r.read().splitlines():
+                    print('Link is unique...posting explanation\n')
+                    comment.reply(header + explanation + footer)
+                    
+                    file_obj_r.close()
+
+                    file_obj_w = open(path,'a+')
+                    file_obj_w.write(comment.id + '\n')
+                    file_obj_w.close()
+                else:
+                    print('Already visited link...no reply needed\n')
+            
+            time.sleep(10)
+
+    print('Waiting 60 seconds...\n')
+    time.sleep(60)
+
+def main():
+    reddit = authenticate()
+    while True:
+        run_explainbot(reddit)
+
+if __name__ == '__main__':
+    main()
